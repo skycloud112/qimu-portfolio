6eefa0583d9575a42a8a87b6db7df53d044ee568
Qi Mu <skycloud112@gmail.com>
2026-01-04 18:06:47 -0500

    update wording

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function HomePage() {
             rel='noopener'
             className='text-primary hover:underline'
           >
-            Medium
+            Medium articles
           </a>
         </div>
 
