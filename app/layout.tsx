bd1f1564bd64d0a239de5a33d45570da448f8d6e
Qi Mu <skycloud112@gmail.com>
2026-01-04 16:28:56 -0500

    add page title

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ import './globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
+      <head>
+        <title>Qi Mu</title>
+      </head>
       <body>
         <ThemeRegistry>
           <Navigation />
